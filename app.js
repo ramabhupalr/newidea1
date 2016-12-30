05bab70ea99ce684360f8ba88422219ba2688c06
Reddy, Ramabhupal <Reddy_Ramabhupal@shure.com>
2016-12-31 01:20:19 +0530

    added angular events

--- app.js
+++ app.js
@@ -9,6 +9,11 @@
     function homeController($log) {
         var vm = this;
         vm.title = "Working Ver Hard!!!"
+        vm.clickCount = 0;
+
+        vm.incrementClikCount = function () {
+             vm.clickCount += 1;   
+        };
 
         activate();
 
